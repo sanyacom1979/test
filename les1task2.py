2d1215cb7c6a1cddbcc327011e49d82ebf032f0a
Alex <sanyacomgm@gmail.com>
2022-06-30 06:35:14 +0300

    решение задач по занятию №1

---
+++ les1task2.py
@@ -0,0 +1,29 @@
+# считывание введенной строки
+m = input()
+# проверка введенных значений
+if int(m) == 1:
+    print("31")
+elif int(m) == 2:
+    print("ФЕВРАЛЬ!")
+elif int(m) == 3:
+    print("31")
+elif int(m) == 4:
+    print("30")
+elif int(m) == 5:
+    print("31")
+elif int(m) == 6:
+    print("30")
+elif int(m) == 7:
+    print("31")
+elif int(m) == 8:
+    print("31")
+elif int(m) == 9:
+    print("30")
+elif int(m) == 10:
+    print("31")
+elif int(m) == 11:
+    print("30")
+elif int(m) == 12:
+    print("31")
+else:
+    print("Должно быть число от 1 до 12")
