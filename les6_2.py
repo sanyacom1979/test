c721551655ba3730afa6509c86491778d6085430
Alex <sanyacomgm@gmail.com>
2022-07-15 21:01:45 +0300

    урок 6 задача 2

---
+++ les6_2.py
@@ -0,0 +1,19 @@
+import json
+import random
+
+with open("wict.json") as js:
+    dict = json.load(js)
+b = 0
+if dict["q1"] == "alex":
+    b += 1
+if dict["q2"] == "moskow":
+    b += 1
+if dict["q3"] == "football":
+    b += 1
+
+print(f"Сумма баллов = {b}")
+
+
+
+
+
