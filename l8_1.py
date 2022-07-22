3758dd26f5d2333cf9c175103dedc8690bdeea22
Alex <sanyacomgm@gmail.com>
2022-07-22 19:38:44 +0300

    занятие 8 задача 1

---
+++ l8_1.py
@@ -0,0 +1,26 @@
+y = int(input("Введите год:"))
+
+if  y % 12 == 0:
+    print("Обезьяна")
+elif y % 12 == 1:
+    print("Петух")
+elif y % 12 == 2:
+    print("Собака")
+elif y % 12 == 3:
+    print("Свинья")
+elif y % 12 == 4:
+    print("Крыса")
+elif y % 12 == 5:
+    print("Бык")
+elif y % 12 == 6:
+    print("Тигр")
+elif y % 12 == 7:
+    print("Кролик")
+elif y % 12 == 8:
+    print("Дракон")
+elif y % 12 == 9:
+    print("Змея")
+elif y % 12 == 10:
+    print("Лошадь")
+elif y % 12 == 11:
+    print("Коза")
