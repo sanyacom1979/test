1279f1b94a2f4d06892ceba3df42a39bd97540fa
Alex <sanyacomgm@gmail.com>
2022-07-29 20:12:44 +0300

    занятие 10 1

---
+++ l10_1.py
@@ -0,0 +1,18 @@
+
+
+def del_comment(src,dst):
+
+    fs = open(src, encoding="utf-8")
+    fd = open(dst, encoding="utf-8", mode="w")
+
+
+    for line in fs:
+        if line[0] != "#":
+            fd.write(line)
+    fs.close()
+    fd.close()
+    print("OK")
+
+src = input("Путь к источнику:")
+dst = input("Путь к назначению:")
+del_comment(src, dst)
