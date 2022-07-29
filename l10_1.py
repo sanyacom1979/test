f9c5841696524491926c30c959b4ccc52d0c3110
Alex <sanyacomgm@gmail.com>
2022-07-29 20:17:12 +0300

    занятие 10 1

--- l10_1.py
+++ l10_1.py
@@ -1,17 +1,18 @@
 
 
 def del_comment(src,dst):
-
-    fs = open(src, encoding="utf-8")
-    fd = open(dst, encoding="utf-8", mode="w")
-
-
-    for line in fs:
-        if line[0] != "#":
-            fd.write(line)
-    fs.close()
-    fd.close()
-    print("OK")
+    try:
+        fs = open(src, encoding="utf-8")
+        fd = open(dst, encoding="utf-8", mode="w")
+    except FileNotFoundError:
+        print("Файл не найден")
+    else:
+        for line in fs:
+            if line[0] != "#":
+             fd.write(line)
+        fs.close()
+        fd.close()
+        print("OK")
 
 src = input("Путь к источнику:")
 dst = input("Путь к назначению:")
